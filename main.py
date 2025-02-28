96037852208137a0e3f0b0ece66b4644846bff8c
SDE <deanisik@gmail.com>
2025-02-28 09:45:01 +0700

    Доделал основной функционал, осталось протестировать

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import os
 import json
 
-command_list = {
-    '1': 'Просмотреть весь список контактов',
-    '2': 'Найти контакт',
-    '3': 'Добавить новый контакт',
-    '4': 'Изменить существующий',
-    '5': 'Удалить существующий контакт',
-    '6': 'Ничего, я передумал, пока:)'
-}
-
 
 def commands():
+    command_list = {
+        '1': 'Просмотреть весь список контактов',
+        '2': 'Найти контакт',
+        '3': 'Добавить новый контакт',
+        '4': 'Изменить существующий',
+        '5': 'Удалить существующий контакт',
+        '6': 'Ничего, я передумал, пока:)'
+    }
     print('Приветствую тебя в телефонном справочнике, что ты хотел сделать?')
     for key, value in command_list.items():
         print(f'{key} - {value}')
@@ -28,7 +27,7 @@ def open_file():
 
 
 def edit_file(data):
-    with open('data_base.json', 'w', encoding='utf-8' ) as file:
+    with open('data_base.json', 'w', encoding='utf-8') as file:
         json.dump(data, file, ensure_ascii=False, indent=4)
 
 
@@ -42,21 +41,31 @@ def view_all():
 
 
 def search_contact():
-    search_param = input('Введите имя или номер телефона: ')
+    search_param = input('Введите порядковый номер, имя или номер телефона: ')
     data = open_file()
     for idx, contact in data.items():
         if search_param == idx:
-            return data[idx]
+            for key, value in data[idx].items():
+                print(f'\t{key:<12} - {value}')
+            return None
         for key, value in contact.items():
             if search_param == value:
-                return data[idx]
+                for keys, values in data[idx].items():
+                    print(f'\t{keys:<12} - {values}')
+                return None
+    return 'Такого контакта не существует'
 
 
-def add_contact(data):
+def add_contact():
+    data = open_file()
     name = input('Имя: ')
     phone_number = input('Номер телефона: ')
     comment = input('Комментарий: ')
-    data[f'{len(data)+1}'] = {
+    key = []
+    for el in data:
+        key.append(int(el))
+    new_key = max(key) + 1
+    data[new_key] = {
         'Имя': name,
         'Телефон': phone_number,
         'Комментарий': comment
@@ -65,14 +74,37 @@ def add_contact(data):
     answer = input('y/n?')
 
     if answer.lower() == 'y':
-        with open('data_base.json', 'w', encoding='utf-8') as file:
-            json.dump(data, file, ensure_ascii=False, indent=4)
+        edit_file(data)
     else:
         return None
 
 
-def change_contact():
-    print('Изменяем')
+def edit_contact():
+    view_all()
+    data = open_file()
+    edit_index = input('Введите ID контакта, который необходимо изменить: ')
+    print('Заполните поле, которое необходимо изменить. В противном случае оставьте пустым.')
+    name = input('Имя: ')
+    if name == '':
+        name = data[edit_index]['Имя']
+    phone_number = input('Номер телефона: ')
+    if phone_number == '':
+        phone_number = data[edit_index]['Телефон']
+    comment = input('Комментарий: ')
+    if comment == '':
+        comment = data[edit_index]['Комментарий']
+    data[edit_index] = {
+        'Имя': name,
+        'Телефон': phone_number,
+        'Комментарий': comment
+    }
+    print(f'Измеить контакт {data[edit_index]['Имя']}?')
+    answer = input('y/n?')
+
+    if answer.lower() == 'y':
+        edit_file(data)
+    else:
+        return None
 
 
 def delete_contact():
@@ -86,20 +118,18 @@ def delete_contact():
         edit_file(data)
 
 
-
 def input_command():
     command = input()
     if command == '1':
         view_all()
     elif command == '2':
         data = search_contact()
-        for key, value in data.items():
-            print(f'\t{key:<12} - {value}')
+        if data:
+            print(data)
     elif command == '3':
-        data = open_file()
-        add_contact(data)
+        add_contact()
     elif command == '4':
-        change_contact()
+        edit_contact()
     elif command == '5':
         delete_contact()
     elif command == '6':
