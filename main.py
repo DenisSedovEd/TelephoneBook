b32b609fb650f94f48d109ab6a4fba2606c19b8c
SDE <deanisik@gmail.com>
2025-03-01 00:28:07 +0700

    edit README file

--- main.py
+++ main.py
@@ -2,38 +2,12 @@ import os
 import json
 
 
-def commands():
-    command_list = {
-        '1': 'Просмотреть весь список контактов',
-        '2': 'Найти контакт',
-        '3': 'Добавить новый контакт',
-        '4': 'Изменить существующий',
-        '5': 'Удалить существующий контакт',
-        '6': 'Ничего, я передумал, пока:)'
-    }
-    print('Приветствую тебя в телефонном справочнике, что ты хотел сделать?')
-    print('_ '*30)
-    for key, value in command_list.items():
-        print(f'{key} - {value}')
-    print('Веди номер команды, которую ты хотел сделать:')
-
-
-commands()
-
-
-def open_file():
-    with open('data_base.json', 'r', encoding='utf-8') as file:
-        data = json.load(file)
-    return data
-
-
-def edit_file(data):
-    with open('data_base.json', 'w', encoding='utf-8') as file:
-        json.dump(data, file, ensure_ascii=False, indent=4)
-
-
-def view_all():
-    data = open_file()
+def show_contacts(data):
+    '''
+    Вывод в консоль всех существующих контактов.
+    :param data:
+    :return:
+    '''
     for idx, contact in data.items():
         print(f'Контакт №{idx}')
         for key, value in contact.items():
@@ -41,7 +15,40 @@ def view_all():
         print('_' * 50)
 
 
+def open_file():
+    '''
+    Открытие json файла с данными.
+    :return:
+    '''
+    with open('data_base.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    return data
+
+
+def edit_file(data):
+    '''
+    Внесение изменений в json файл.
+    :param data:
+    :return:
+    '''
+    with open('data_base.json', 'w', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
+
+
+def view_all():
+    '''
+    Пункт меню №1. Вывод всех контактов.
+    :return:
+    '''
+    data = open_file()
+    show_contacts(data)
+
+
 def search_contact():
+    '''
+    Пункт меню №2. Поиск контакта по ID, имени или номеру телефона.
+    :return:
+    '''
     search_param = input('Введите порядковый номер, имя или номер телефона: ')
     data = open_file()
     for idx, contact in data.items():
@@ -58,6 +65,10 @@ def search_contact():
 
 
 def add_contact():
+    '''
+    Пункт меню №3. Добавление нового контакта.
+    :return:
+    '''
     data = open_file()
     name = input('Имя: ')
     phone_number = input('Номер телефона: ')
@@ -86,36 +97,50 @@ def add_contact():
 
 
 def edit_contact():
-    view_all()
+    '''
+    Пункт меню №4. Внесение изменений в существующий контакт.
+    Выбор производится по ID.
+    :return:
+    '''
     data = open_file()
+    show_contacts(data)
     edit_index = input('Введите ID контакта, который необходимо изменить: ')
     print('Заполните поле, которое необходимо изменить. В противном случае оставьте пустым.')
     name = input('Имя: ')
     if name == '':
         name = data[edit_index]['Имя']
     phone_number = input('Номер телефона: ')
+    if not phone_number.isdigit():
+        phone_number = input('Номер телефона не может содержать буквы или специальные символы. Введите еще раз.\n')
+        if not phone_number.isdigit():
+            print('Некорректно введен номер телефона.')
+            return None
     if phone_number == '':
         phone_number = data[edit_index]['Телефон']
     comment = input('Комментарий: ')
     if comment == '':
         comment = data[edit_index]['Комментарий']
-    data[edit_index] = {
-        'Имя': name,
-        'Телефон': phone_number,
-        'Комментарий': comment
-    }
     print(f'Измеить контакт {data[edit_index]['Имя']}?')
     answer = input('y/n?')
 
     if answer.lower() == 'y' or answer.lower() == 'н':
+        data[edit_index] = {
+            'Имя': name,
+            'Телефон': phone_number,
+            'Комментарий': comment
+        }
         edit_file(data)
     else:
         return None
 
 
 def delete_contact():
-    view_all()
+    '''
+    Пункт меню №5. Удаление контакта по ID.
+    :return:
+    '''
     data = open_file()
+    show_contacts(data)
     del_index = input('Введите ID контакта, который необходимо удалить: ')
     print(f'Удалить указанный контакт?')
     answer = input('y/n?')
@@ -125,9 +150,25 @@ def delete_contact():
 
 
 def input_command():
-    command = input()
+    '''
+    Функция меню. Выводит доступные пользователю команды.
+    :return:
+    '''
+    command_list = {
+        '1': 'Просмотреть весь список контактов',
+        '2': 'Найти контакт',
+        '3': 'Добавить новый контакт',
+        '4': 'Изменить существующий',
+        '5': 'Удалить существующий контакт',
+        '6': 'Ничего, я передумал, пока:)'
+    }
+    print('Приветствую тебя в телефонном справочнике, что ты хотел сделать?')
+    print('_ ' * 30)
+    for key, value in command_list.items():
+        print(f'{key} - {value}')
+    command = input('Веди номер команды, которую ты хотел сделать:\n')
+    os.system('cls')
     if command == '1':
-        os.system('cls')
         view_all()
     elif command == '2':
         data = search_contact()
@@ -140,7 +181,7 @@ def input_command():
     elif command == '5':
         delete_contact()
     elif command == '6':
-        print('Как хочешь, пока')
+        print('Ну как хочешь, пока')
     else:
         print('Такой команды нету...')
 
